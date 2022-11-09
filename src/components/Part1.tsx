11a75cf9487d5005d10a3e2f715cbf80b475dfab
Valeria <valeriasadik@hotmail.com>
2022-11-09 12:55:36 +0000

    Update Part1.tsx

--- src/components/Part1.tsx
+++ src/components/Part1.tsx
@@ -6,6 +6,7 @@ const Part1 = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
   const [allComents, setAllComments] = useState([]);
 
   const feedBackGood = () => {
